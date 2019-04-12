50b66a2ea029e03c6569a175f7ed8ff48f5d2ffb
unknown <benzantortizb@student.mville.edu>
2019-04-12 09:21:36 -0400

    Work on Shop & Inventory Coding for the weekend

---
+++ Autocell/Assets/Scripts/UI/Shop.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Shop : MonoBehaviour
+{
+	private Text[] cellCategory;
+	private QuadCell[] cell;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    	if(cell!=null) {
+	    	List<QuadCell.index> index = new List<QuadCell.index>();
+	        for(int i = 0; i <= index.Count; i++) {
+
+	        }
+	    }
+    }
+}
